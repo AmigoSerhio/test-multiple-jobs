c3cac6a1eb11c7dbf3e15cb8f47c66d72869d58e
AZANIR <eleoneks@gmail.com>
2021-03-29 16:30:11 +0300

    another changes in project

--- test/specs/example.e2e.js
+++ test/specs/example.e2e.js
@@ -8,6 +8,6 @@ describe("My Login application", () => {
     LoginPage.login("tomsmith", "SuperSecretPassword!");
     expect(SecurePage.flashAlert).toBeExisting();
     expect(SecurePage.flashAlert).toHaveTextContaining("You logged into a secure area!");
-    console.log("news changet in project");
+    console.log("new change in project");
   });
 });

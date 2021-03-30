de2f6699cda7f2a8b0cbb0bbb7475feddcbfe495
Leonid Mayevskiy <eleoneks@gmail.com>
2021-03-30 12:45:11 +0300

    19-43

--- test/specs/example.e2e.js
+++ test/specs/example.e2e.js
@@ -3,11 +3,11 @@ const SecurePage = require("../pageobjects/secure.page");
 
 describe("My Login application", () => {
   it("should login with valid credentials", () => {
-    LoginPage.open();
+    // LoginPage.open();
 
-    LoginPage.login("tomsmith", "SuperSecretPassword!");
-    expect(SecurePage.flashAlert).toBeExisting();
-    expect(SecurePage.flashAlert).toHaveTextContaining("You logged into a secure area!");
+    // LoginPage.login("tomsmith", "SuperSecretPassword!");
+    // expect(SecurePage.flashAlert).toBeExisting();
+    // expect(SecurePage.flashAlert).toHaveTextContaining("You logged into a secure area!");
     console.log("new change in project");
     console.log("new branch add");
     console.log("fix bug");

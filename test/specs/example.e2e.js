4af967cf471e987d21c67fb92db23d15e8d34809
AZANIR <eleoneks@gmail.com>
2021-04-06 11:10:57 +0300

    add spec

--- test/specs/example.e2e.js
+++ test/specs/example.e2e.js
@@ -8,6 +8,9 @@ describe("My Login application", () => {
     LoginPage.login("tomsmith", "SuperSecretPassword!");
     expect(SecurePage.flashAlert).toBeExisting();
     expect(SecurePage.flashAlert).toHaveTextContaining("You logged into a secure area!");
+  });
+
+  it("jshdfgkjhg", () => {
     browser.url("https://artstroy.net");
   });
 });
